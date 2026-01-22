98418b6fd78df3866a7a52a47d842f515a63c16c
Uday Kiran <udaykiranuk1126@gmail.com>
2026-01-22 16:52:17 +0530

    Wrote student functions and added a new grade ND (Not Declared or Unknown)

--- student.c
+++ student.c
@@ -1,3 +1,70 @@
 #include "student.h"
 
-// TO DO: Implement createStudent,modifyStudent,deleteStudent functions
+// Function to create a new student
+Student *create_student(int id, const char *name)
+{
+    Student *new_student = (Student *)malloc(sizeof(Student));
+
+    snprintf(new_student->id, sizeof(new_student->id), "%d", id);
+    strncpy(new_student->name, name, sizeof(new_student->name));
+    
+    return new_student;
+}
+
+void set_student_marks(Student *student, int subject_index, float minor_marks, float major_marks)
+{
+    if (subject_index < 0 || subject_index >= SUBS_COUNT)
+    {
+        fprintf(stderr, "Invalid subject index\n");
+        return;
+    }
+
+    student->minorScores[subject_index] = minor_marks;
+    student->majorScores[subject_index] = major_marks;
+}
+
+void set_student_grade(Student *student, Grade grade)
+{
+    student->totalGrade = grade;
+}
+
+const char *get_student_id(const Student *student)
+{
+    return student->id;
+}
+
+const char *get_student_name(const Student *student)
+{
+    return student->name;
+}
+
+float get_student_total_marks(const Student *student)
+{
+    return student->totalMarks;
+}
+
+float get_student_percentage(const Student *student)
+{
+    return student->totalPercentage;
+}
+
+Grade get_student_grade(const Student *student)
+{
+    return student->totalGrade;
+}
+
+Grade get_student_subject_grade(const Student *student, int subject_index)
+{
+    if (subject_index < 0 || subject_index >= SUBS_COUNT)
+    {
+        fprintf(stderr, "Invalid subject index\n");
+        return ND;
+    }
+
+    return student->subject_grades[subject_index];
+}
+
+void delete_student(Student *student)
+{
+    free(student);
+}
--- student.h
+++ student.h
@@ -17,7 +17,8 @@ typedef enum
     B,
     C,
     D,
-    F
+    F,
+    ND
 } Grade;
 
 typedef struct
@@ -27,10 +28,23 @@ typedef struct
     float minorScores[SUBS_COUNT];
     float majorScores[SUBS_COUNT];
     float totalScores[SUBS_COUNT];
+    Grade subject_grades[SUBS_COUNT];
     float totalMarks;
     float totalPercentage;
-    Grade grade;
+    Grade totalGrade;
 } Student;
 
+Student *create_student(int id, const char *name);
+void set_student_marks(Student *student, int subject_index, float minor_marks, float major_marks);
+void set_student_grade(Student *student, Grade grade);
+
+const char * get_student_id(const Student *student);
+const char *get_student_name(const Student *student);
+float get_student_total_marks(const Student *student);
+float get_student_percentage(const Student *student);
+Grade get_student_grade(const Student *student);
+Grade get_student_subject_grade(const Student *student, int subject_index);
+
+void delete_student(Student *student);
 
 #endif
